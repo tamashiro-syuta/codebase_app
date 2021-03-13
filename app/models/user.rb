ddb4065d34a15c6c59791be671b164185f3a1aa8
tamashiro_syuta <syuta5825@gmail.com>
2021-03-13 22:06:57 +0900

    ログイン機能（完成）

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :validatable
+  
+  validates :username, presence: true, length: { maximum: 50 }
+
+end
