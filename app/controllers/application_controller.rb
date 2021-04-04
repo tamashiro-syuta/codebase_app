6bbf1abd714a1facb67d8228fb223c56290794bd
tamashiro_syuta <syuta5825@gmail.com>
2021-04-04 21:58:00 +0900

    ログイン画面をデフォルトに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_out, keys: [:username])
   end
 
-  # ログイン後のリダイレクト先
-  def after_sign_in_path_for(resource)
-    user_path(resource.id)
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/:id' => "users#show"
-  # resources :users
   resources :posts
   devise_for :users
-  root 'pages#index'
+  root 'users#index'
   get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
